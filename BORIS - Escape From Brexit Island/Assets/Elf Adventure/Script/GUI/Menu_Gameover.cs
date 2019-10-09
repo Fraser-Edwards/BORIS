4f9b209c93ebd2e7c9a66195c229f447e1475a06
Stuart Docherty <stuartdocherty@gmail.com>
2019-10-09 13:05:52 +0200

    Added a flag for the win level point, fixed gameover screen to not show the "continue" button.

--- BORIS - Escape From Brexit Island/Assets/Elf Adventure/Script/GUI/Menu_Gameover.cs
+++ BORIS - Escape From Brexit Island/Assets/Elf Adventure/Script/GUI/Menu_Gameover.cs
@@ -4,16 +4,11 @@ using UnityEngine.UI;
 
 public class Menu_Gameover : MonoBehaviour {
 	public Text liveText;
-	public GameObject Next;
 	public GameObject StartOver;
 	public GameObject Buttons;
 	int lives;
 
 	void Awake(){
-		if (LevelManager.Instance.isLastLevelOfWorld)
-			Next.SetActive (false);		//dont show the next button when this is the final level
-
-
 
 	}
 
@@ -29,11 +24,6 @@ public class Menu_Gameover : MonoBehaviour {
 
 
 		var levelReached = PlayerPrefs.GetInt (GlobalValue.worldPlaying.ToString (), 1);
-
-		if (GlobalValue.levelPlaying <= levelReached)
-			Next.SetActive (true);
-		else
-			Next.SetActive (false);
 		
 		liveText.text = (lives + 1).ToString ("00");
 		StartCoroutine (SubtractLiveCo (1));
